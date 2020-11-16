f48170764afcc7ca58a3713ceb3392bf441eef18
Amelle El Bakkal <amelle.el.bakkal@octo.com>
2020-11-16 12:17:23 +0100

    ajoute test pour valeur négative

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -6,7 +6,27 @@ describe('Gilded Rose', function () {
     it('should foo', function() {
         const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items[0].name).to.equal('foo');
+    });
+
+    it('should decrease the quality by one ',  function() {
+        // Given
+        const gildedRose = new GildedRose([new Item('lala', 0, 1)]);
+
+        // When
+        const itemsUpdated = gildedRose.updateQuality();
+        // Then
+        expect(itemsUpdated[0].quality).to.equal(0);
+    });
+
+    it('should not have a negative quality ',  function() {
+        // Given
+        const gildedRose = new GildedRose([new Item('lala', 0, 0)]);
+
+        // When
+        const itemsUpdated = gildedRose.updateQuality();
+        // Then
+        expect(itemsUpdated[0].quality).to.equal(0);
     });
 
 });
